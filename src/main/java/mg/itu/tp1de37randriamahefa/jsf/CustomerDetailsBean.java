94e943925d0e671c888ed6ba72a17d65375b32b3
Fanantenana R <80602341+FanantenanaR@users.noreply.github.com>
2024-03-24 21:13:21 +0300

    Création de Discount Manager, lister les discount dans customerDetails

--- src/main/java/mg/itu/tp1de37randriamahefa/jsf/CustomerDetailsBean.java
+++ src/main/java/mg/itu/tp1de37randriamahefa/jsf/CustomerDetailsBean.java
@@ -8,8 +8,11 @@ import jakarta.inject.Named;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
 import java.io.Serializable;
+import java.util.List;
 import mg.itu.tp1de37randriamahefa.entity.Customer;
+import mg.itu.tp1de37randriamahefa.entity.Discount;
 import mg.itu.tp1de37randriamahefa.service.CustomerManager;
+import mg.itu.tp1de37randriamahefa.service.DiscountManager;
 
 /**
  *
@@ -21,9 +24,13 @@ public class CustomerDetailsBean implements Serializable {
 
     private int idCustomer;
     private Customer customer;
+    private List<Discount> discounts;
 
     @Inject
     private CustomerManager customerManager;
+    
+    @Inject
+    private DiscountManager discountManager;
 
     public int getIdCustomer() {
         return idCustomer;
@@ -34,8 +41,8 @@ public class CustomerDetailsBean implements Serializable {
     }
 
     /**
-     * Retourne les détails du client courant (contenu dans l'attribut customer de
-     * cette classe).
+     * Retourne les détails du client courant (contenu dans l'attribut customer
+     * de cette classe).
      */
     public Customer getCustomer() {
         return customer;
@@ -44,7 +51,9 @@ public class CustomerDetailsBean implements Serializable {
     /**
      * Action handler - met à jour dans la base de données les données du client
      * contenu dans la variable d'instance customer.
-     * @return la prochaine page à afficher, celle qui affiche la liste des clients.
+     *
+     * @return la prochaine page à afficher, celle qui affiche la liste des
+     * clients.
      */
     public String update() {
         // Modifie la base de données.
@@ -55,5 +64,16 @@ public class CustomerDetailsBean implements Serializable {
 
     public void loadCustomer() {
         this.customer = customerManager.findById(idCustomer);
+    }
+
+    /**
+     * Retourne la liste de tous les Discount.
+     * @return 
+     */
+    public List<Discount> getDiscounts() {
+        if (discounts == null) {
+            discounts = discountManager.getAllDiscounts();
+        }
+        return discounts;
     }
 }
